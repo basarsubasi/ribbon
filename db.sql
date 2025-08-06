db1b614991fe7520d4aa95b0558f4d60c874977f
basar <basarsubasi@protonmail.com>
2025-08-06 23:55:41 +0300

    database simplified

--- db.sql
+++ db.sql
@@ -4,6 +4,9 @@ CREATE TABLE books (
     book_id INTEGER PRIMARY KEY AUTOINCREMENT,
     book_type TEXT NOT NULL,
     title TEXT NOT NULL,
+    author TEXT NOT NULL,
+    category TEXT NOT NULL,
+    publisher TEXT NOT NULL,
 	cover_url TEXT,
 	cover_path TEXT,
     number_of_pages INTEGER NOT NULL,
@@ -17,54 +20,11 @@ CREATE TABLE books (
     review TEXT,
 	notes TEXT,
 	stars INTEGER, 
-	price REAL,
-	
-);
-
-CREATE TABLE categories (
-    category_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    category_name TEXT NOT NULL UNIQUE
+	price REAL
 );
 
 
 
-CREATE TABLE book_categories (
-    book_id INTEGER NOT NULL,
-    category_id INTEGER NOT NULL,
-    PRIMARY KEY (book_id, category_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE,
-    FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE CASCADE
-);
-
-
-
-CREATE TABLE authors (
-    author_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    author_name TEXT NOT NULL UNIQUE
-);
-
-CREATE TABLE book_authors (
-    book_id INTEGER NOT NULL,
-    author_id INTEGER NOT NULL,
-    PRIMARY KEY (book_id, author_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE,
-    FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE
-);
-
-
-CREATE TABLE publishers (
-    publisher_id INTEGER PRIMARY KEY AUTOINCREMENT,
-    publisher_name TEXT NOT NULL UNIQUE
-);
-
-
-CREATE TABLE book_publishers (
-    book_id INTEGER NOT NULL,
-    publisher_id INTEGER NOT NULL,
-    PRIMARY KEY (book_id, publisher_id),
-    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE,
-    FOREIGN KEY (publisher_id) REFERENCES publishers(publisher_id) ON DELETE CASCADE
-);
 
 CREATE TABLE page_logs (
     page_log_id INTEGER PRIMARY KEY AUTOINCREMENT,
