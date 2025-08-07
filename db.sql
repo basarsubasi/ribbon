113c0d041a95b6916dfeea7f7e1cb3f76aa83bba
basar <basarsubasi@protonmail.com>
2025-08-07 14:58:07 +0300

    revert the database simplification

--- db.sql
+++ db.sql
@@ -4,11 +4,8 @@ CREATE TABLE books (
     book_id INTEGER PRIMARY KEY AUTOINCREMENT,
     book_type TEXT NOT NULL,
     title TEXT NOT NULL,
-    author TEXT NOT NULL,
-    category TEXT NOT NULL,
-    publisher TEXT NOT NULL,
-	cover_url TEXT,
-	cover_path TEXT,
+    cover_url TEXT,
+    cover_path TEXT,
     number_of_pages INTEGER NOT NULL,
     isbn_13 TEXT UNIQUE,
     openlibrary_code TEXT UNIQUE, 
@@ -18,10 +15,53 @@ CREATE TABLE books (
     last_read  TIMESTAMP,
     current_page INTEGER NOT NULL DEFAULT 0,
     review TEXT,
-	notes TEXT,
-	stars INTEGER, 
-	price REAL
+    notes TEXT,
+    stars INTEGER, 
+    price REAL
 );
+
+
+
+CREATE TABLE authors (
+    author_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE categories (
+    category_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL
+);
+
+CREATE TABLE publishers (
+    publisher_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    name TEXT UNIQUE NOT NULL
+);
+
+
+CREATE TABLE book_authors (
+    book_id INTEGER NOT NULL,
+    author_id INTEGER NOT NULL,
+    PRIMARY KEY (book_id, author_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE,
+    FOREIGN KEY (author_id) REFERENCES authors(author_id) ON DELETE CASCADE
+);
+
+CREATE TABLE book_categories (
+    book_id INTEGER NOT NULL,
+    category_id INTEGER NOT NULL,
+    PRIMARY KEY (book_id, category_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE,
+    FOREIGN KEY (category_id) REFERENCES categories(category_id) ON DELETE CASCADE
+);
+
+CREATE TABLE book_publishers (
+    book_id INTEGER NOT NULL,
+    publisher_id INTEGER NOT NULL,
+    PRIMARY KEY (book_id, publisher_id),
+    FOREIGN KEY (book_id) REFERENCES books(book_id) ON DELETE CASCADE,
+    FOREIGN KEY (publisher_id) REFERENCES publishers(publisher_id) ON DELETE CASCADE
+);
+
 
 
 
